fb66f2c0645eaa92d4421c640eec2b182086b7a3
Dracko <joseph13rios@hotmail.com>
2022-10-03 23:21:33 -0500

    -digo yo que ya esta listo para probar totalmente

--- declaracionVariables.py
+++ declaracionVariables.py
@@ -3,8 +3,8 @@ import re
 
 clase="((extern|static|register) +)?"
 tipo="(short int|int|long int|bool|float|double|long double)"#agregar el espacio entre tipo u nombre de variable
-reservadas="(?!(int|short int|long int|bool|float|double|long double|const|if|else|while|switch|case|break|for|do|static|extern|register|auto))"
-indent="[^0-9]*"+reservadas+"([a-z]|[A-Z]|_)+"#([a-z]|[A-Z]|_|[0-9])*"#falta evitar que se ingresen palabras reservadas
+reservadas="(?!((int|short int|long int|bool|float|double|long double|const|if|else|while|switch|case|break|for|do|static|extern|register|auto)(\s|;)+))"
+indent=reservadas+"([a-z]|[A-Z]|_)+([a-z]|[A-Z]|_|[0-9])*;"#falta evitar que se ingresen palabras reservadas
 exp="(=[0-9]+(.[0-9]+)*)?;"
 #{1} indica que debe haber solo 1 repeticion
 #? indica que puede haber 0 o no mas de 1 repeticion
