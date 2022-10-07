98c2b0d446a85f8b35667dc798089a3ba1c1db65
Dracko <joseph13rios@hotmail.com>
2022-10-07 10:00:01 -0500

    agregado el if y las condicionales

--- declaracionVariables.py
+++ declaracionVariables.py
@@ -1,20 +1,38 @@
 import re
 
-from numpy import integer
-
-
-clase="((extern|static|register) +)?"
-tipo="(short int|int|long int|bool|float|double|long double)"#agregar el espacio entre tipo u nombre de variable
-reservadas="(?!((int|short int|long int|bool|float|double|long double|const|if|else|while|switch|case|break|for|do|static|extern|register|auto)(\s|;)+))"
-indent=reservadas+"([a-z]|[A-Z]|_)+([a-z]|[A-Z]|_|[0-9])*"
-exp="((\s)*=(\s)*[0-9]+(.[0-9]+)*)?;"
 #{1} indica que debe haber solo 1 repeticion
 #? indica que puede haber 0 o no mas de 1 repeticion
 #?! es una forma de negacion que indica que no puede estar presente una cadena en una posicion indicada
-variables=" *"+clase+tipo+" +("+indent+";|"+indent+exp+")"
+
+
+
+clase="((extern|static|register) +)?" #clases de la variable
+tipo="(short int|int|long int|bool|float|double|long double)"#tipo de dato
+reservadas="(?!((int|short int|long int|bool|float|double|long double|const|if|else|while|switch|case|break|for|do|static|extern|register|auto)(\s|;)+))"#palabras reservadas
+variable=reservadas+"([a-z]|[A-Z]|_)+([a-z]|[A-Z]|_|[0-9])*"#nombre de la variable
+operadoresA="(\*|\+|/|-|>|==|<|¡=|<=|>=|=)"#operadores aritmeticos
+operadoresL="(&&|\|{2})"
+exp="((\s)*=(\s)*)?;"#expresion de asignación MEJORAR
+
+
+#Expresion regular completa para la declaración de variables numericas
+declaracion=" *"+clase+tipo+" +("+variable+";|"+variable+exp+")"
+#condSimp = una variable sola, una variable o numero, seguido de un operador aritmetico seguido de una variable o 
+#           numero una o mas veces 
+condSimp="("+variable+"|("+variable+"|[0-9]+(.[0-9]+)?)(\s)*("+operadoresA+"("+variable+"|[0-9]+(.[0-9]+)?)(\s)*)+)"
+
+#condComp = a una condicion simple o condicion simpleentre parentesis, seguida de un operador logico, seguido de una condicion simple entre parentesis, repetida una o mas veces 
+condComp="(\("+condSimp+"\)(\s)*"+operadoresL+"\("+condSimp+"\)(\s)*)"
+condicion="("+condSimp+"|"+condComp+"+)"
+
+#expresion regular completa para la condicional if 
+CondicionalIF="if\("+condicion+"\)"
+
+#expresion regular completa para la condicional while 
+bucleWhile="while\("+condicion+"\)"
 
 cadena = input("Ingrese la cadena: ")
-result = re.fullmatch(variables, cadena)
+result = re.fullmatch(CondicionalIF, cadena)
 if result:
     print("Cadena aceptada")
 else:
